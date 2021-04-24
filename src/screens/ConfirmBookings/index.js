3d9607fd893d30f1e108c2ea604378b392d109dd
Federico <federico.ariganello@fizzmod.com>
2021-04-24 11:28:07 -0300

    added basic dependencies and two basic screens

---
+++ src/screens/ConfirmBookings/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const CreateBookings = () => (
+    <>
+        <div>CONFIRM BOOKINGS</div>
+        <Link to="/">Go Back</Link>
+    </>
+)
+
+export default CreateBookings;
---
+++ src/screens/CreateBookings/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const CreateBookings = () => (
+    <>
+        <div>CREATE BOOKINGS</div>
+        <Link to="/confirm">Confirm bookings</Link>
+    </>
+)
+
+export default CreateBookings;
